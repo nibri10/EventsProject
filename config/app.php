172c457e6f10d7f61275deb0771f3d45278cfcca
Nicolas Oliveira <nibri.kcond2011@gmail.com>
2018-11-04 19:40:25 -0200

    Fix problem in app.php

--- config/app.php
+++ config/app.php
@@ -210,11 +210,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> Adding Listerner and Events
     ],
 
 ];
